9aef8ef056d264733a23d6d7d9e7d3eae362e2f4
Elena Rickert <elena.simone.rickert.2007@gmail.com>
2023-11-02 19:49:34 -0500

    Revert "Merge branch 'Integration' into Drive"
    This reverts commit 353f65f7c985af21dc11c00c252b196be60eabe1, reversing
    changes made to 046fb27a59cd7a6b4e9a355269a5505280aa3d6c.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,15 +5,81 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. 
+ * This class should not be used for any other purpose. 
+ * All constants should be declared globally (i.e. public static). 
+ * Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  // RoboRio and CAN ports
+
+  public static final class CAN {
+    // FRC DEFAULTS
+    public static final int PDP = 1; // for rev
+    public static final int PCM1 = 2; // for rev
+
+  }
+
+  // PWM assignments on the Rio
+  public static final class PWM {
+    public static final int DT_FL = 1;
+    public static final int DT_BL = 2;
+    public static final int DT_FR = 3;
+    public static final int DT_BR = 4;
+
+  }
+
+  // Digital IO on the RIO
+  public static final class DigitalIO {
+    //DIO Ports for encoders
+    public static final int DT_LEFT_ENCODER_A = 1;
+    public static final int DT_LEFT_ENCODER_B = 2;
+  }
+
+  public static final class AnalogIn {
+    // public static final int MAGAZINE_ANGLE = 0;
+  }
+
+  // PWM assignments on the Rio
+  public static final class PCM1 {
+    // static final int <name> = 0;
+
+  }
+
+  // if we use a second PCM
+  public static final class PCM2 {
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static final class DriverControls {
+
+    public enum Id {
+      Driver(0), Operator(1), SwitchBoard(2), Phantom(3);
+
+      public final int value;
+
+      Id(int value) {
+        this.value = value;
+      }
+    }
+
+    public enum DriverMode {
+      Arcade(0), Tank(1), XYRot(2);
+
+      public final int value;
+
+      DriverMode(int value) {
+        this.value = value;
+      }
+    }
   }
 }

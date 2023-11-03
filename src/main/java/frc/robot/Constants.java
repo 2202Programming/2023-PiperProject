de8c2e3b5598c70d67d80b037997cc083b03cbda
Elena Rickert <elena.simone.rickert.2007@gmail.com>
2023-11-02 19:41:30 -0500

    Revert "Merge branch 'intakeTest' into main"
    This reverts commit c5ebc6409388773fc4b95687f605fddd2b88083f, reversing
    changes made to bca2dce5d3db4b684a06f237825767af32486069.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,92 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. 
- * This class should not be used for any other purpose. 
- * All constants should be declared globally (i.e. public static). 
- * Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-
-  // RoboRio and CAN ports
-
-  public static final class CAN {
-    // FRC DEFAULTS
-    public static final int PDP = 1; // for rev
-    public static final int PCM1 = 2; // for rev
-
-  }
-
-  public static final class PWM {
-    public static final int DT_FL = 1;
-    public static final int DT_BL = 2;
-    public static final int DT_FR = 3;
-    public static final int DT_BR = 4;
-
-    //intake motor 1 and motor 2 are the same motor, but one object is bound to motor controller 9 
-    //and the other to 10, even though it's the same motor -ER
-
-   // electrically this controller goes to 9 & 10
-    public static final int Intake_Motors = 9;
-    
-    
-    
-  }
-
-  // Digital IO on the RIO
-  public static final class DigitalIO {
-    //DIO Ports for encoders
-    public static final int DT_LEFT_ENCODER_A = 1;
-    public static final int DT_LEFT_ENCODER_B = 2;
-  }
-
-  public static final class AnalogIn {
-    // public static final int MAGAZINE_ANGLE = 0;
-  }
-
-  // PWM assignments on the Rio
-  public static final class PCM1 {
-
-    // dual solenoid for intake
-    public static final int intakeForward = 0;
-    public static final int intakeReverse = 1;
-
-
-  }
-
-  // if we use a second PCM
-  public static final class PCM2 {
-  }
-
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-  }
-
-  public static final class DriverControls {
-
-    public enum Id {
-      Driver(0), Operator(1), SwitchBoard(2), Phantom(3);
-
-      public final int value;
-
-      Id(int value) {
-        this.value = value;
-      }
-    }
-
-    public enum DriverMode {
-      Arcade(0), Tank(1), XYRot(2);
-
-      public final int value;
-
-      DriverMode(int value) {
-        this.value = value;
-      }
-    }
   }
 }
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,16 +4,14 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(Intake subsystem) {
-    //return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-    // commented above to let code compile ^^
-    return null; 
+  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
 
   private Autos() {
